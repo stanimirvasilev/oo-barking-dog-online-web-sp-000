996641f3c87c9cfea9fcb9d132a854e171839103
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-12 12:49:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,16 @@
 # Your code goes here!
 class Dog 
   
-  def name =()
+  def name =(dog_name)
+    @this_dog_name
   end
   
-  def
+  def name
+    @this_dog_name
   end
-  
   
   def bark 
     puts "woof!"
   end
-  
-  
-  
-  
   
 end
